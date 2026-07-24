4fa694fa3d3219e7cca993a24952520c32a0aae3
Jhamende <jonathanhamende@gmail.com>
2026-07-24 23:51:24 +0200

    Enable ttyd toolbar with bottom and resume buttons

--- codex_terminal/run.sh
+++ codex_terminal/run.sh
@@ -34,6 +34,9 @@ export LC_ALL=C.UTF-8
 export TERM=xterm-256color
 export COLORTERM=truecolor
 
+printf 'Preparing ttyd interface with quick-action toolbar\n'
+/usr/local/bin/prepare-ttyd-index
+
 printf 'Starting classic ttyd terminal on port 8099\n'
 printf 'Working directory: %s\n' "$WORKING_DIRECTORY"
 printf 'Font size: %s\n' "$FONT_SIZE"
@@ -43,6 +46,7 @@ exec ttyd \
   --port 8099 \
   --writable \
   --signal SIGTERM \
+  --index /tmp/ttyd-index.html \
   --client-option "fontSize=${FONT_SIZE}" \
   --client-option "scrollback=50000" \
   --client-option "disableLeaveAlert=true" \
